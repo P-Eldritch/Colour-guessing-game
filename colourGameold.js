0cd8159502cd8de0a41daafd2bb80558a95f10dd
P-Eldritch <juodojitulzis@gmail.com>
2017-11-10 02:23:38 +0200

    hex mode added

--- colourGameold.js
+++ colourGameold.js
@@ -8,19 +8,26 @@ var colours= generateRandomcolours(squaresCount);
 var squares = document.querySelectorAll(".square");
 var pickedColourDisplay=document.querySelector("#colourDisplay");
 var pickedColour=pickColour();
+var typeMarker;
 
-start();
+
+buttonsEvents();
+squaresEvents();
+
 //gives new colours
 newGame.addEventListener("click", function(){
 	restart();
+	typeMarker = "rgb";
 });
 //alert about hex
 hex.addEventListener("click", function(){
-	alert("hex version will be available soon");
+	restart("hex");
+	typeMarker = "hex";
 });
 
-function start() {
-//controls selected button; calls restart to generate colours accordingly
+
+function buttonsEvents() {
+//controls selected button; calls restart to generate colours accordingly (3 or 6)
 	for (var i=0; i<easyhardButtons.length; i++) {
 	easyhardButtons[i].addEventListener("click", function(){
 		easyhardButtons[0].classList.remove("selected");
@@ -35,14 +42,22 @@ function start() {
 	restart();
 	});
 	}
-//grab colour of clicked square, compare colour to picked colour
+}
+//grab colour of the clicked square, compare the colour to a picked colour
+function squaresEvents() {
 	for(var i=0; i<squares.length; i++){
 		squares[i].addEventListener ("click", function(){
 		var clickedColour = this.style.backgroundColor;
 		if (pickedColour===clickedColour){
 			changeColours(clickedColour);
 			message.textContent = "Correct!";
-			newGame.textContent = "Play Again?";
+			if (typeMarker === "hex") {
+				hex.textContent = "Play Again";
+				newGame.textContent = "New rgb";
+				}
+			else {
+				newGame.textContent = "Play Again";
+				hex.textContent = "New hex"}
 			h1.style.backgroundColor = clickedColour;
 			}
 		else{
@@ -55,13 +70,19 @@ function start() {
 	restart();
 }
 
-function restart(){
+function restart(type){
 	colours = generateRandomcolours(squaresCount);
 	pickedColour =pickColour();
-	colourDisplay.textContent = pickedColour;
 	h1.style.backgroundColor="#74d600";
 	message.textContent = "";
-	newGame.textContent = "New colours"
+	hex.textContent = "New hex"
+	newGame.textContent = "New rgb"
+	if (type === "hex") {
+		colourDisplay.textContent = rgbToHex(pickedColour);
+	}
+	else {
+		colourDisplay.textContent = pickedColour;
+	}
 	for (var i=0; i<squares.length;i++) {
 		if (colours[i]) {
 			squares[i].classList.remove("hidden");
@@ -72,13 +93,12 @@ function restart(){
 		}
 	}
 }
-//setting up the new colours button
 function changeColours(colour) {
 	for (var i=0; i<squares.length; i++) {
 		squares[i].style.backgroundColor = colour;
 	}
 }
-
+//generates as many random numbers as there are places in colours array; picks one colour for guessing
 function pickColour(){
 	var random = Math.floor(Math.random()*colours.length);
 	return colours[random];
@@ -86,15 +106,25 @@ function pickColour(){
 
 function generateRandomcolours(num) {
 	var arr =[];
+	//fill array with strings, containing rgb values
 	for (var i=0; i<num; i++) {
-		//anonymous function generates rgb values and is stored in arr[i]
+		//anonymous function generates rgb values and values are stored in arr[i]
 		arr[i]=	(function() {
 			var r = Math.floor(Math.random()*256);
 			var g = Math.floor(Math.random()*256);
 			var b = Math.floor(Math.random()*256);
-			//adding spaces, otherwise it does not match clicked colour (coming from css background colour - dom or smth else adds spaces)
+			//adding spaces, otherwise it does not match clicked colour (coming from css backgroundColor property)
 			return "rgb(" + r + ", " + g + ", " + b + ")"; 
 			})();
 	};
 	return arr;
+}
+function rgbToHex(num) {
+	var hexNumArray = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+	var splitArr = num.replace('rgb(','').replace(')','').split(", ");
+	var stringy = "hex #";
+	for (var i = 0; i<splitArr.length; i++) {
+		stringy = stringy + hexNumArray[(splitArr[i] - splitArr[i] % 16)/16] +  hexNumArray[splitArr[i] % 16]
+	}
+	return stringy;
 }
